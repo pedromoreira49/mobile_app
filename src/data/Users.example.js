6fd045bf39d3f37e917c06308959ffbc11122f8a
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-02-25 18:49:09 -0300

    feat: add Users mock data example

---
+++ src/data/Users.example.js
@@ -0,0 +1,8 @@
+export const datas = [
+    {
+        id: "1",
+        nome: 'Pedro Moreira',
+        username: 'pedromoreira49',
+        avatar: 'https://avatars.githubusercontent.com/u/62686343?v=4',
+    }
+]
